9bdf0955b9f0c7d6bebc8c79df1cf0043c14caef
marinakalmykova <marina@meyvndigital.co.uk>
2020-05-11 18:45:27 +0300

    добавление статистики

--- src/components/filter.js
+++ src/components/filter.js
@@ -6,18 +6,18 @@ const getFilterNameById = (id) => {
   return id.substring(FILTER_ID_PREFIX.length);
 };
 
-const createFilterMarkup = (filter, isChecked) => {
-
+const createFilterMarkup = (filter) => {
+  const {name, isChecked, isDisabled} = filter;
   return (
     `<div class="trip-filters__filter">
-                <input id="filter-${filter.name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${filter.name}" ${isChecked ? `checked` : ``}>
-                <label class="trip-filters__filter-label" for="filter-${filter.name}">${filter.name}</label>
+                <input id="filter-${name}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${name}" ${isChecked ? `checked` : ``}${isDisabled ? ` disabled` : ``}>
+                <label class="trip-filters__filter-label" for="filter-${name}">${name}</label>
               </div>`
   );
 };
 
 const createFilterTemplate = (filters) => {
-  const filtersMarkup = filters.map((it) => createFilterMarkup(it, it.checked)).join(`\n`);
+  const filtersMarkup = filters.map((it) => createFilterMarkup(it)).join(`\n`);
   return (
     `<form class="trip-filters" action="#" method="get">
               ${filtersMarkup}
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import FilterController from "./controllers/filter.js";
 import TripCostComponent from "./components/trip-cost.js";
 import TripController from "./controllers/trip";
 import PointsModel from "./models/points.js";
+import StatisticsComponent from "./components/statistics.js";
 import {generateEvents} from "./mock/point.js";
 import {render, RenderPosition} from "./utils/render.js";
 import {EVENTS_COUNT} from "./const.js";
+import {Tabs} from "./components/menu";
 
 const points = generateEvents(EVENTS_COUNT);
 const pointsModel = new PointsModel();
@@ -35,9 +37,10 @@ const countTripPrice = () => {
 const siteMainElement = document.querySelector(`.trip-main`);
 render(siteMainElement, new TripInfoComponent(), RenderPosition.AFTERBEGIN);
 
+const menuComponent = new MenuComponent();
 const tripControlElement = document.querySelector(`.trip-controls`);
 const tripMenuElement = tripControlElement.querySelector(`.trip-tabs`);
-render(tripMenuElement, new MenuComponent(), RenderPosition.AFTEREND);
+render(tripMenuElement, menuComponent, RenderPosition.AFTEREND);
 
 const tripInfoElement = document.querySelector(`.trip-info`);
 render(tripInfoElement, new TripCostComponent(countTripPrice()), RenderPosition.BEFOREEND);
@@ -47,8 +50,12 @@ const filterController = new FilterController(tripFilterElement, pointsModel);
 filterController.render();
 
 const tripEventsElement = document.querySelector(`.trip-events`);
+const statisticsComponent = new StatisticsComponent(pointsModel);
+render(tripEventsElement, statisticsComponent, RenderPosition.BEFOREEND);
 const tripController = new TripController(tripEventsElement, pointsModel);
+statisticsComponent.hide();
 tripController.render();
+
 
 const newEventButton = document.querySelector(`.trip-main__event-add-btn`);
 
@@ -56,4 +63,17 @@ newEventButton.addEventListener(`click`, () => {
   tripController.createPoint();
   filterController.render();
   newEventButton.disabled = true;
+});
+
+menuComponent.setOnChange((tab) => {
+  switch (tab) {
+    case Tabs.TABLE:
+      statisticsComponent.hide();
+      tripController.show();
+      break;
+    case Tabs.STATS:
+      tripController.hide();
+      statisticsComponent.show();
+      break;
+  }
 });
