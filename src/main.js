30964e51ab8e9d46f52a4ac0ca46a8adeee0eb9a
marinakalmykova <marina@meyvndigital.co.uk>
2020-05-31 01:04:39 +0300

    сервис-воркер

--- src/main.js
+++ src/main.js
@@ -93,6 +93,15 @@ apiWithProvider.getDestinations()
     tripController.render();
   });
 
+window.addEventListener(`load`, () => {
+  navigator.serviceWorker.register(`/sw.js`)
+    .then(() => {
+      // Действие, в случае успешной регистрации ServiceWorker
+    }).catch(() => {
+    // Действие, в случае ошибки при регистрации ServiceWorker
+    });
+});
+
 window.addEventListener(`online`, () => {
   document.title = document.title.replace(` [offline]`, ``);
 
